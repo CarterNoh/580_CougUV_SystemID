89240daa3ea72e7e0b70948b18f6e7e66853281c
BlakePR <blakepromrell@gmail.com>
2024-11-29 16:54:39 -0700

    test the actuator dynamics function

--- test_coug.py
+++ test_coug.py
@@ -35,3 +35,24 @@ def test_override_params_bad():
     with pytest.raises(ValueError):
         coug1.override_params(params)
     
+def test_actuator_dynamics():
+    coug1 = coug.Coug()
+
+    command = np.array([1,1,1,1])
+    timesteprudder = coug1.T_delta
+    timestep_propeller = coug1.T_n
+
+    assert timesteprudder == timestep_propeller
+
+    new_u_actual, new_u_actual_dot = coug1.actuator_dynamics(timesteprudder, command, coug1.u_actual)
+    assert np.all(np.isclose(new_u_actual,command))
+    assert np.all(np.isclose(new_u_actual_dot,(1/timestep_propeller)*command))
+
+    coug1.u_actual = np.array([0,0,0,0],dtype=float)
+    first_u_dot = new_u_actual_dot.copy()
+
+    timesteprudder, timestep_propeller = timesteprudder / 2, timestep_propeller /2
+    new_u_actual,new_u_actual_dot = coug1.actuator_dynamics(timesteprudder, command, coug1.u_actual)
+    assert np.all(np.isclose(new_u_actual, command / 2))
+    assert np.all(np.isclose(new_u_actual_dot,first_u_dot))
+
